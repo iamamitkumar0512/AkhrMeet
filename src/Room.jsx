92f1ebb6b7be683a76c96ea082682290960e6704
unknown <iamamitkumar051@gmail.com>
2023-09-25 13:06:34 +0530

    bug fix

--- src/Room.jsx
+++ src/Room.jsx
@@ -191,16 +191,11 @@ const Room = () => {
             )}
           </div>
           {disconnect && (
-            <button
-              className="m-4 text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
-              onClick={() => {
-                setTimeout(() => {
-                  navigate("/");
-                }, 1000);
-              }}
-            >
-              DISCONNECT
-            </button>
+            <a href="/">
+              <button className="m-4 text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
+                DISCONNECT
+              </button>
+            </a>
           )}
         </div>
       </div>
